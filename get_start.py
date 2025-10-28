c5e00d1ecc65e657583982a31c3fcad661636b6e
Rania Ben Salah <raniabensalah02@proton.me>
2025-10-28 22:59:29 +0000

    changing the whole code

--- get_start.py
+++ get_start.py
@@ -1,1 +1,13 @@
-print("hello world")
+import sys 
+import random
+
+for i in range (10):
+    test = random.randint(2,5)
+
+    rand=int (input("msg: "))
+
+    if rand == test :
+        print ('rbe7tm3ana frigidair ')
+
+    else : 
+        print ("yalla mara jeya ")
